da0cecf24d807149d56ecb87131827731edb2530
Ngọ Văn Trọng <ngovantrong1308@gmail.com>
2025-03-07 17:44:33 +0700

    đồng bộ cấu trức thư mục cho ae FE

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import os
 from flask import Flask, render_template
-from BackEnd.Controller.bacsi_control import bacsi_bp
 app = Flask(__name__, template_folder="templates")  # Chỉ định thư mục chứa HTML
-app.register_blueprint(bacsi_bp)
+
 @app.route("/")
 def home():
-    return render_template("BacSiView.html")
+    return render_template("BenhNhanView.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
