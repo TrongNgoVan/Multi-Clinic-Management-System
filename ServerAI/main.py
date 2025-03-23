0130612f20fee59eeb56a7e1dae9f734386bbf20
Ngọ Văn Trọng <ngovantrong1308@gmail.com>
2025-03-23 15:44:31 +0700

    thay cổng

--- ServerAI/main.py
+++ ServerAI/main.py
@@ -68,4 +68,4 @@ def ai_diagnosis():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    app.run(host="26.80.253.0", port=5001, debug=True)
