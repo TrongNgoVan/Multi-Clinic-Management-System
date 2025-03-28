8badfe023e655e4a5d0eb875f0690ac5b40415f7
Ngọ Văn Trọng <ngovantrong1308@gmail.com>
2025-03-28 15:15:18 +0700

    fix UI bác sĩ.....

--- ServerBussiness/main.py
+++ ServerBussiness/main.py
@@ -41,3 +41,7 @@ app.register_blueprint(phongchucnang_bp, url_prefix="/phongchucnang")
 
 if __name__ == "__main__":
     app.run(host="26.80.253.0", port=5000)
+
+# chạy node.js server cho fe live-server --host=26.80.253.0 --port=5500
+#  ip wifi trường 172.11.34.197
+#  ip trọng
